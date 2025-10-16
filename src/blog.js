e255938ea6c93f368d55c94b8b1c27a921e12dca
Lorinc <lheutchy@gmail.com>
2025-10-15 23:27:01 -0700

    feat: add html pages for blogs and fix code spacing

--- src/blog.js
+++ src/blog.js
@@ -1,7 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * Declares an array named `blogs`.
  */
-var blogs = [
+const blogs = [
     {
         title: "Taking A Peek",
         date: "09/22/2011",
@@ -22,28 +24,29 @@ var blogs = [
 /**
  * Dynamically appends blogs to the webpage using DOM Manipulation.
  */
-var blogContainer = document.getElementById("blog-container");
-blogs.forEach(function (Blog) {
-    var title = document.createElement("h2");
+const blogContainer = document.getElementById("blog-container");
+blogs.forEach((Blog) => {
+    const title = document.createElement("h2");
     title.textContent = Blog.title;
-    var image = document.createElement("img");
+    const image = document.createElement("img");
     image.src = Blog.image;
     image.alt = Blog.imageAlt;
     image.style.width = "300px";
     image.style.height = "auto";
-    var date = document.createElement("p");
+    const date = document.createElement("p");
     date.textContent = Blog.date;
-    var description = document.createElement("p");
+    const description = document.createElement("p");
     description.textContent = Blog.description;
-    var slug = document.createElement("a");
-    slug.href = "blogs/".concat(Blog.slug, ".html");
+    const slug = document.createElement("a");
+    slug.href = `blogs/${Blog.slug}.html`;
     slug.textContent = "Read About It Here";
-    var blogDiv = document.createElement("div");
+    const blogDiv = document.createElement("div");
     blogDiv.classList.add("blog-post");
     blogDiv.appendChild(title);
     blogDiv.appendChild(image);
     blogDiv.appendChild(date);
     blogDiv.appendChild(description);
     blogDiv.appendChild(slug);
-    blogContainer === null || blogContainer === void 0 ? void 0 : blogContainer.appendChild(blogDiv);
+    blogContainer?.appendChild(blogDiv);
 });
+//# sourceMappingURL=blog.js.map
